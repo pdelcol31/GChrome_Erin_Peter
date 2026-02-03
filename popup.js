a0e64f73bac4e26aa2e004759773c673e0ef3159
Peter Del Col/usr/local/bin/python3 <pdelcol@haverford.edu>
2026-02-03 11:20:15 -0500

    Edits added texts and tokens

--- popup.js
+++ popup.js
@@ -1,44 +1,32 @@
-let scrapeChat = document.getElementById('scrapeChat');
+import "tiktoken";
 let getModel = document.getElementById('getModel');
 
-let list = document.getElementById('emaillist');
-let mList = document.getElementById('modelList');
+let list = document.getElementById('response');
+
+parameters = 800000;
 
 // Handler to receive Models from content Script
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse)=> {
 
-    // get emails
-    let emails = request.emails;
-    
+    //get responses
     let responses = request.contents;
 
-    if (responses == null || responses.length == 0) {
-        // no response
-        let li2 = document.createElement("li2");
-        li2.innerText = "No response found";
-        list.appendChild(li2);
-    } else {
-        //
-        responses.forEach((response) => {
-            let li2 = document.createElement("li2");
-            li2.innerText = response;
-            list.appendChild(li2);s
-        });
-    }
-    // Display emails on popup
-    if (emails == null || emails.length == 0) {
-        // no emails
-        let li = document.createElement("li");
-        li.innerText = "No emails found";
+    num_tokens = num_tokens_from_string(responses, "gpt-4o-mini"); // will need to pull model somehow
+
+    alert(num_tokens);
+
+    //display responses in popup
+    if(responses == null || responses.length == 0){
+        //No response found
+        let li = document.createElement('li');
+        li.innerText = "No response Found";
         list.appendChild(li);
-    } else {
-        //display emails
-        emails.forEach((email) => {
-            let li = document.createElement("li");
-            li.innerText = email;
-            list.appendChild(li);
-        });
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerText = responses;
+        list.appendChild(li);
     }
 });
 
@@ -54,40 +42,17 @@ getModel.addEventListener("click", async () => {
 
 function scrapeModelResponse(){
     
-    const response = document.queryselector(".whitespace-pre-wrap");
+    const response = document.querySelectorAll("p");
 
-    alert("response");
-
-    const contents = response.innerHTML;
+    const contents = Array.from(response)
+        .map(p => p.textContent)
+        .join(" ");
 
     chrome.runtime.sendMessage({contents});
 }
 
-// Button's click event listener
-scrapeChat.addEventListener("click", async () => {
 
-    // Get current active tab
-    let [tab] = await chrome.tabs.query({active: 
-    true, currentWindow: true});
-
-    // Execute script to parse emails on page
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func:  scrapeChatResponse,
-    });
-})  
-
-function scrapeChatResponse(){
-    
-    //RegEx to parse emails from html code
-    const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
- 
-    // Parse emails from the HTML of the page
-    let emails = document.body.innerHTML.match(emailRegEx);
-
-    // send emails to popup
-    chrome.runtime.sendMessage({emails});
-} 
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
-//
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+//https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
