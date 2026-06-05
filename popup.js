b2396a43be2147c4331a52ca455700ef9413d96d
Erin Dougherty <edougherty@haverford.edu>
2026-06-05 15:56:00 -0400

    changing popup

--- popup.js
+++ popup.js
@@ -65,16 +65,55 @@
 
 document.addEventListener("DOMContentLoaded", async ()=> {
     const userDiv = document.getElementById('userFootprint');
-    const button = document.getElementById('gptFootprintButton');
+    const linkButton = document.getElementById('gptFootprintButton');
+    const welcomeButton = document.getElementById('welcomeButton');
+    const totTokensButton = document.getElementById('totTokensButton');
+    const avgTokensButton = document.getElementById('avgTokensButton');
+    const totCarbonButton = document.getElementById('totCarbonButton');
+    const avgCarbonButton = document.getElementById('avgCarbonButton');
+    const milesButton = document.getElementById('milesButton');
+    const totWaterButton = document.getElementById('totWaterButton');
+    const avgWaterButton = document.getElementById('avgWaterButton');
+    const bottlesButton = document.getElementById('bottlesButton');
 
     // Example: Update the text immediately on load
     //I think we can mess with this to update with an incoming message somehow?
     const result = await chrome.storage.local.get(["user_data"]);
     const userData = result.user_data || "Your Data will update as you use the extension";
+    console.log("incoming message:" + result.user_data);
+    userDataChars = ['','','','','','','','',''];
+    index = 0;
+    for (const char of userData) {
+        if (char == '|'){
+            index += 1;
+        }
+        else{
+            userDataChars[index] += char;
+        }
+    }
+    // const welcome_message = userDataChars[0];
+    // const total_tokens = userDataChars[1];
+    // const avg_tokens = userDataChars[2];
+    // const total_carbon = userDataChars[3];
+    // const avg_carbon = userDataChars[4];
+    // const total_miles = userDataChars[5];
+    // const total_water = userDataChars[6];
+    // const avg_water = userDataChars[7];
+    // const total_bottles = userDataChars[8];
 
-    document.getElementById("userdata").textContent = userData;
 
-    button.addEventListener('click', function() {
+    // document.getElementById("userdata").textContent = userData;
+    welcomeButton.innerText = userDataChars[0];
+    totTokensButton.innerText = userDataChars[1];
+    avgTokensButton.innerText = userDataChars[2];
+    totCarbonButton.innerText = userDataChars[3];
+    avgCarbonButton.innerText = userDataChars[4];
+    milesButton.innerText = userDataChars[5];
+    totWaterButton.innerText = userDataChars[6];
+    avgWaterButton.innerText = userDataChars[7];
+    bottlesButton.innerText = userDataChars[8];
+
+    linkButton.addEventListener('click', function() {
         chrome.tabs.create({ url: 'http://gptfootprint.cs.haverford.edu/' });
     });
 
